e8703e0ea4e0073624eda36c6106b6d84adeb232
Hossain Sakhaout <hSakhaout@clearoneadvantage.com>
2022-07-12 00:49:25 -0400

    Add method in the BaseClass

--- src/main/java/sonic.vap.base/BaseClass.java
+++ src/main/java/sonic.vap.base/BaseClass.java
@@ -6,33 +6,49 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
     private static final Logger logger = LogManager.getLogger(BaseClass.class);
-    protected static final String CHROME_BROWSER = "chrome";
-    protected static final String EDGE_BROWSER = "edge";
     public static WebDriver driver = null;
-    protected static String SET_BROWSER = null;
+    public static int PAGE_LOAD_TIMEOUT = 15;
 
-    protected static void driverInitialization(String browser, Object capabilities) {
-        SET_BROWSER = browser;
+    private static void driverInitialization(Object capabilities) {
         try {
-            switch (browser.toLowerCase()) {
-                case EDGE_BROWSER:
-                    driver = new EdgeDriver((EdgeOptions) capabilities);
-                    break;
-
-                case CHROME_BROWSER:
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver((ChromeOptions) capabilities);
-                    break;
-            }
-            logger.info("Browser is Opening: " + browser.toUpperCase());
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver((ChromeOptions) capabilities);
+            logger.info("Browser is Opening");
         } catch (Exception e) {
             e.printStackTrace();
-            logger.error("NOT able to open the Browser: " + browser.toUpperCase());
+            logger.error("NOT able to open the Browser");
         }
+    }
+
+    private static ChromeOptions browsePreSetUp() {
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        options.addArguments("--incognito");
+        options.addArguments("--headless");
+        options.addArguments("window-size=1920,681");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-extensions");
+        return options;
+    }
+
+    private static void driverSetUp(String url) {
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.get(url);
+        logger.info("URL Entered: " + url);
+    }
+
+    public static void driverInitialize() {
+        driverInitialization(browsePreSetUp());
+        driverSetUp("https://sonicvapeusa.com/");
     }
 }
